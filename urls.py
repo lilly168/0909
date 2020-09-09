9e300d5d1abfd7620ac8a6b3a7c94b986fe46222
이나리 <mrnr96@gmail.com>
2020-09-09 19:58:32 +0900

    0909 두번째

--- urls.py
+++ urls.py
@@ -7,3 +7,7 @@ urlpatterns = [
     path('update/<int:pk>/', views.update, name='update'),
     path('delete/<int:pk>/', views.delete, name='delete'),
 ]
+    path('comment/update/<int:pk>', views.comment_update,
+    name='comment_update'), 
+    path('comment/delete/<int:pk>', views.comment_delete,
+    name='comment_delete'), 
--- views.py
+++ views.py
@@ -48,3 +48,28 @@ def delete(request, pk):
     blog = Blog.objects.get(pk=pk)
     blog.delete()
     return redirect('home')
+
+def comment_update(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    blog = get_object_or_404(Blog, pk=comment.blog.id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('/blog/'+str(blog.id))
+        
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, 'blog/comment_update.html', {'form': form})
+    
+def comment_delete(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    blog = get_object_or_404(Blog, pl=comment.blog.id)
+
+    if request.method == 'POST':
+        comment.delete()
+        return redirect('/blog/'+str(blog.id))
+    else:
+        return render(request, 'blog/comment_delete.html', {'object': comment})
+        
